5f817aaa35f9881e6874366b2738f748d617be7d
Deezie <deshawnjones036@gmail.com>
2024-02-20 20:22:53 -0600

    first merch tee

---
+++ hello.py
@@ -0,0 +1,32 @@
+# Define two numbers for the operations
+num1 = 10
+num2 = 5
+
+# Addition
+addition_result = num1 + num2
+print("Addition:", addition_result)
+
+# Subtraction
+subtraction_result = num1 - num2
+print("Subtraction:", subtraction_result)
+
+# Multiplication
+multiplication_result = num1 * num2
+print("Multiplication:", multiplication_result)
+
+# Division
+division_result = num1 / num2
+print("Division:", division_result)
+
+# Integer Division
+integer_division_result = num1 // num2
+print("Integer Division:", integer_division_result)
+
+# Modulus (Remainder)
+modulus_result = num1 % num2
+print("Modulus (Remainder):", modulus_result)
+
+# Exponentiation
+exponentiation_result = num1 ** num2
+print("Exponentiation:", exponentiation_result)
+
